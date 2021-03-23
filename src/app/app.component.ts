101e74e00c2cd301af99da7d3877806824dc239b
Andrey <cattalin.htc@gmail.com>
2021-03-23 09:59:04 +0100

    peliculas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'demo3';
+
+  showBlue = true;
+  showGreen = false;
+
+  txtButton = "Mostrar mensaje"
+
+  newPelicula = ""
+  allPeliculas = ["Iron Man", "Superman 2", "Spiderman", "Wonder Woman"]
+
+  changeGreen() {
+    this.showGreen = !this.showGreen
+
+    if(this.showGreen) {
+      this.txtButton = "Ocultar mensaje"
+    } else {
+      this.txtButton = "Mostrar mensaje"
+    }
+  }
+
+  addPelicula(data: any) {
+    this.allPeliculas.push(data.value)
+    data.value = ""
+  }
+
+  deletePeli(position: number) {
+    console.log("Quiero eliminar la posicion " + position + " que corresponde a " + this.allPeliculas[position]);
+
+    // splice primer parametro es de la posicion que quiero eliminar
+    // y el segundo, cuantos quiero eliminar
+    this.allPeliculas.splice(position,1)
+  }
+
+
 }
